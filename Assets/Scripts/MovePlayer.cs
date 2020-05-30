ae2f0bb9c0e0715b1f29b08a489948b36e0fdde4
dodecaphonix <dodecaphonix@gmail.com>
2020-05-30 00:30:33 -0700

    Add Player, Terrain, and MovePlayer

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+	public float speed = 30;
+
+	void FixedUpdate() {
+        float verticalPress = Input.GetAxisRaw ("Vertical");
+        float horizontalPress = Input.GetAxisRaw ("Horizontal");
+        GetComponent<Rigidbody>().velocity = new Vector3(horizontalPress, 0, verticalPress) * speed;
+	}
+
+    // Start is called before the first frame update
+    void Start() {}
+
+    // Update is called once per frame
+    void Update() {}
+}
